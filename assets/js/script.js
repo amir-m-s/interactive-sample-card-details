aac6c63f1b8f246b1ea619d40bc71f9d64205b24
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-09-26 10:55:39 +0330

    Add cardNumber validation and cardNumber display functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ const MM = document.querySelector("#mm");
 const YY = document.querySelector("#yy");
 const cvc = document.querySelector("#cvc");
 
+const cardFrontDefaultNumber = "0000 0000 0000 0000";
 
 // Confirm variable
 const confirmBtn = document.querySelector("#confirm-btn");
@@ -14,7 +15,8 @@ const confirmBtn = document.querySelector("#confirm-btn");
 // Declare name validation function
 function nameValidation() {
   const nameLength = cardName.value.length;
-  if (nameLength < 3) {
+  const hasNum = /\d/.test(cardName.value);
+  if (nameLength < 3 || hasNum) {
     cardName.classList.add("invalid-input");
     cardName.nextElementSibling.classList.add("visible");
   } else {
@@ -25,4 +27,37 @@ function nameValidation() {
 // Add name validation event listener to cardName input
 cardName.addEventListener("change", nameValidation);
 
-// function 
+// number validation function
+function numberValidation() {
+  const hasNaN = isNaN(cardNumber.value);
+  if (hasNaN) {
+    cardNumber.classList.add("invalid-input");
+    cardNumber.nextElementSibling.classList.add("visible");
+  } else {
+    cardNumber.classList.remove("invalid-input");
+    cardNumber.nextElementSibling.classList.remove("visible");
+  }
+  numberFormatter();
+}
+
+cardNumber.addEventListener("input", numberValidation);
+
+function numberFormatter() {
+  const cardFrontNumber = document.querySelector(".card-front .card-number");
+
+  let cardNumberArray = [];
+  for (let i = 0; i < cardNumber.value.length; i += 4) {
+    cardNumberArray.push(cardNumber.value.substring(i, i + 4));
+    let soFar = cardNumberArray.join(" ");
+    soFar = soFar.length;
+    console.log(soFar);
+  }
+
+  const res = cardNumberArray.join(" ");
+
+  if (cardNumber.value.length === 0 ) {
+    cardFrontNumber.innerText = cardFrontDefaultNumber;
+  } else {
+    cardFrontNumber.innerText = res;
+  }
+}
